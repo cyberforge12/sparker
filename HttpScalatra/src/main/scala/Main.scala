0886621720f48d1a288d44d66bb3a3d58c9eb3da
Василий Горемыкин <vasilijgoremykin@MacBook-Pro-Vasilij.local>
2020-11-24 11:06:50 +0300

    conn_st is a param

--- HttpScalatra/src/main/scala/Main.scala
+++ HttpScalatra/src/main/scala/Main.scala
@@ -1,4 +1,5 @@
 object Main extends App with LazyLogging {
+  val conn_st = args(0)
   val server = HttpServer.buildWebService(8080, classOf[WebService])
   logger.info("Starting server")
   server.start()
