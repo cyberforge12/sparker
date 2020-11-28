84f12155cc275278f1b7eed4d5b0b61209641ae6
Василий Горемыкин <vasilijgoremykin@MacBook-Pro-Vasilij.local>
2020-11-28 14:33:02 +0300

    rm http

--- HttpScalatra/src/main/scala/Main.scala
+++
@@ -1,7 +0,0 @@
-object Main extends App with LazyLogging {
-  val conn_st = args(0)
-  val server = HttpServer.buildWebService(8080, classOf[WebService])
-  logger.info("Starting server")
-  server.start()
-  logger.info("Server started at " + server.getURI)
-}
